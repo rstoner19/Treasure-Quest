45fde3b999e7d13720bc5e49503b40c2a37396f7
Rick Stoner <stonerrick19@gmail.com>
2016-08-04 16:34:08 -0700

    story boards with crude layout completed, viewcontroller files created

---
+++ Treasure-Quest/Treasure-Quest/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  Treasure-Quest
+//
+//  Created by Rick  on 8/4/16.
+//  Copyright © 2016 Michael Sweeney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
