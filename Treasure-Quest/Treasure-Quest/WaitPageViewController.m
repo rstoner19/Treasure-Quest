2de9b3f7877a0f195d05954c71134d13942d0c4c
Rick Stoner <stonerrick19@gmail.com>
2016-08-06 10:10:58 -0700

    added some items for wait screen

---
+++ Treasure-Quest/Treasure-Quest/WaitPageViewController.m
@@ -0,0 +1,38 @@
+//
+//  WaitPageViewController.m
+//  Treasure-Quest
+//
+//  Created by Rick  on 8/5/16.
+//  Copyright © 2016 Michael Sweeney. All rights reserved.
+//
+
+#import "WaitPageViewController.h"
+
+@interface WaitPageViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *questNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gameCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *gameDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation WaitPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupViewController];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+- (void)setupViewController {
+    self.questNameLabel.text = @"Quest Name Variable Here";
+    self.gameCodeLabel.text = @"game Code var";
+    self.gameDescriptionLabel.text = @"description here";
+    
+}
+
+@end
