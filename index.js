25c8c99f6fd990d4e621614aadeb1ffad4b72a09
Derek Graham <derekgraham@mac.com>
2016-08-06 00:32:28 -0700

    changes for cloud functionality

--- index.js
+++ index.js
@@ -11,16 +11,36 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+if (process.env.APNS_ENABLE) {
+  pushConfig['ios'] = [
+    {
+      pfx: 'ParsePushDevelopmentCertificate.p12', // P12 file only
+      bundleId: 'beta.codepath.parsetesting',  // change to match bundleId
+      production: false // dev certificate
+    }
+  ];
+}
+
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  appId: process.env.APP_ID || 'tresQ',
+  masterKey: process.env.MASTER_KEY || 'bestGroup', //Add your master key here. Keep it secret!
+  push: pushConfig,
+  filesAdapter: filesAdapter,
+  serverURL: process.env.SERVER_URL || 'http://localhost/parse'  // needed for Parse Cloud and push notifications
 });
+
+// var api = new ParseServer({
+//   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+//   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+//   appId: process.env.APP_ID || 'tresQ',
+//   masterKey: process.env.MASTER_KEY || 'bestGroup', //Add your master key here. Keep it secret!
+//   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+//   liveQuery: {
+//     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+//   }
+// });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -48,7 +68,7 @@ app.get('/test', function(req, res) {
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+  console.log('parse-server-example running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
